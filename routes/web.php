6450f4fe073c880033ecc45a353e269643bf4d7e
Garrett Cox <garrettjcox@gmail.com>
2019-05-05 09:57:54 -0500

    Remove unused blade templates

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,3 @@
 */
 
 Route::get('/{any}', 'VueController@index')->where('any', '^(?!api).*$');
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Auth::routes(['verify' => true]);
-
-// Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
